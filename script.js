8f48d52c941b47d5a401d45e7c0276169f90416f
fallenflames <79skyblazexd@gmail.com>
2026-01-22 19:10:34 +0800

    Initial commit: Timezone tracker project

---
+++ script.js
@@ -0,0 +1,93 @@
+//user time----------------------------------------------------
+
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+document.getElementById('user_zone_text').textContent = userTimeZone;
+
+
+function updateClock() {
+    const now = new Date();
+
+    const timeString = now.toLocaleTimeString();
+    document.getElementById('user_time').textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+//GMT time----------------------------------------------------
+
+function updateGMTClock() {
+    const now = new Date();
+    const gmtTime = now.toLocaleTimeString('en-US', {
+        timeZone: 'UTC'
+    });
+    document.getElementById('gmt_time').textContent = gmtTime;
+}
+
+updateGMTClock();
+setInterval(updateGMTClock, 1000);
+
+//CET time----------------------------------------------------
+
+function updateCETClock() {
+    const now = new Date();
+    const cetTime = now.toLocaleTimeString('en-US', {
+        timeZone: 'Europe/Paris'
+    });
+    document.getElementById('cet_time').textContent = cetTime;
+}
+
+updateCETClock();
+setInterval(updateCETClock, 1000);
+
+//GST time----------------------------------------------------
+
+function updateGSTClock() {
+    const now = new Date();
+    const gstTime = now.toLocaleTimeString('en-US', {
+        timeZone: 'Atlantic/South_Georgia'
+    });
+    document.getElementById('gst_time').textContent = gstTime;
+}
+
+updateGSTClock();
+setInterval(updateGSTClock, 1000);
+
+//IST time----------------------------------------------------
+
+function updateISTClock() {
+    const now = new Date();
+    const istTime = now.toLocaleTimeString('en-US', {
+        timeZone: 'Asia/Kolkata'
+    });
+    document.getElementById('ist_time').textContent = istTime;
+}
+
+updateISTClock();
+setInterval(updateISTClock, 1000);
+
+//AEST time----------------------------------------------------
+
+function updateAESTClock() {
+    const now = new Date();
+    const aestTime = now.toLocaleTimeString('en-US', {
+        timeZone: 'Australia/Sydney'
+    });
+    document.getElementById('aest_time').textContent = aestTime;
+}
+
+updateAESTClock();
+setInterval(updateAESTClock, 1000);
+
+//NZDT time----------------------------------------------------
+
+function updateNZDTClock() {
+    const now = new Date();
+    const nzdtTime = now.toLocaleTimeString('en-US', {
+        timeZone: 'Pacific/Auckland'
+    });
+    document.getElementById('nzdt_time').textContent = nzdtTime;
+}
+
+updateNZDTClock();
+setInterval(updateNZDTClock, 1000);
